92cc51e6bc0fe976e650add8156c79e5c7874086
shinji46kuro <shinji.46.kuro@gmail.com>
2020-04-04 15:05:40 +0900

    Initial Commit

---
+++ NeumorphismSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeumorphismSwiftUI
+//
+//  Created by Shinji Kurosawa on 2020/04/04.
+//  Copyright © 2020 Shinji Kurosawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
